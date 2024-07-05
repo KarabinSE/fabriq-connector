ff2067b47ef8bb0cccfbeab565d488fb8db6a9b2
Johannes Arvidsson <41383171+cleanSumo@users.noreply.github.com>
2024-07-05 10:08:57 +0200

    added GetNewsItemRequest (#2)

---
+++ src/Requests/GetNewsItemRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Karabin\FabriqConnector\Requests;
+
+use Illuminate\Support\Facades\Cache;
+use Saloon\CachePlugin\Contracts\Cacheable;
+use Saloon\CachePlugin\Contracts\Driver;
+use Saloon\CachePlugin\Drivers\LaravelCacheDriver;
+use Saloon\CachePlugin\Traits\HasCaching;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetNewsItemRequest extends Request implements Cacheable
+{
+    use HasCaching;
+
+    /**
+     * The HTTP method of the request
+     */
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        protected readonly string $slug,
+        public array $params = []
+    ) {
+        if (! config('fabriq-connector.enable_cache')) {
+            $this->disableCaching();
+        }
+    }
+
+    protected function defaultQuery(): array
+    {
+        return $this->params;
+    }
+
+    protected function cacheKey(): ?string
+    {
+        return 'fabriq_articles';
+    }
+
+    /**
+     * The endpoint for the request
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/news/'.$this->slug;
+    }
+
+    public function resolveCacheDriver(): Driver
+    {
+        return new LaravelCacheDriver(Cache::store(config('fabriq-connector.cache_store')));
+    }
+
+    public function cacheExpiryInSeconds(): int
+    {
+        return config('fabriq-connector.cache_expiry');
+    }
+}
